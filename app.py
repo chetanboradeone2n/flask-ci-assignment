df642460e4821a6c18efe5dba6a771e52fe9c3bb
One2N <one2n@One2Ns-MacBook-Air.local>
2025-07-31 11:28:39 +0530

    Test runner2

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-students = []
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask!"
+    return "Hello, CI Pipeline is working!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
